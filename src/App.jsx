c29c0149dbc81b489da32bd0a2408c9ca3f49409
adixshrma1 <aditya.codez26@gmail.com>
2025-10-05 17:21:31 +0530

    hero animation implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  useGSAP(() => {
-    gsap.to(".box", {
-      x: 350,
-      rotation: 180,
-      repeat: -1,
-      yoyo: true,
-    });
-  }, []);
-
   return (
-    <div className="flex-center h-screen">
-      <h1>hello gsap</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-screen"></div>
+    </main>
   );
 };
 
